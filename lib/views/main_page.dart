5168938d767f35f20b62daa8a2b172edd742334f
Jacob Root <otis@otisroot.com>
2022-05-26 22:03:00 -0700

    Added bottom bar for main page

--- lib/views/main_page.dart
+++ lib/views/main_page.dart
@@ -8,15 +8,60 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
+  int _selectedIndex = 0;
+
+  static const List<Widget> _pages = <Widget>[
+    Center(
+      child: Icon(
+        Icons.book,
+        size: 150,
+      ),
+    ),
+    Center(
+      child: Icon(
+        Icons.library_books,
+        size: 150,
+      ),
+    ),
+    Center(
+      child: Icon(
+        Icons.settings,
+        size: 150,
+      ),
+    ),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Icon(
-          Icons.laptop,
-          size: 250,
-        )
-      )
+    return Scaffold(
+      body: IndexedStack(
+        index: _selectedIndex,
+        children: _pages,
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.menu_book_sharp),
+            label: 'Reading Now',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.library_books),
+            label: 'Library',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: 'Settings',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+      ),
     );
   }
 }
